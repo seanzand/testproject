cd51951bf022d9787a0e1bb7a9f9c1798e34e256
Sean <seanzandbergen@gmail.com>
2018-10-15 22:05:38 +0200

    user registration

---
+++ accounts/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages,auth
+from django.contrib.auth.models import User
+def register(request):
+    if request.method == 'POST':
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        #Check if password match
+        if password == password2:
+            #Check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email already taken')
+                    return redirect('register')
+                else:
+                    #Looks good
+                    user = User.objects.create_user(username=username,
+                    password=password,
+                    email=email,
+                    first_name=first_name,
+                    last_name=last_name)
+                    user.save()
+                    messages.success(request,'You are now registered and can log in')
+                    return redirect('login')
+                    
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
+
+def login(request):
+    if request.method == 'POST':
+        #login user
+        return
+    else:
+        return render(request, 'accounts/login.html')
+
+def logout(request):
+    return redirect('index')
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
